96dba0c60b40aec27aa174f8db5ab5f226961b75
J.Bravo <joepbravo@gmail.com>
2021-04-19 18:55:22 -0700

    This is a commit attempt

---
+++ testupload_1.jl
@@ -0,0 +1,9 @@
+# Used the following code to try and determine question 1 part 1.
+count( ==(1), termfreq )
+# Per the code above, the number of elements that only appeared once is 2,648.
+
+apperances_word = 0 .< termfreq
+
+counts = sum( appearances_word, dim = 1 )
+
+#This is just a test line to make sure second upload is okay.
