a4f4edd8121dff6c11058eafdf2107f0cd88cff9
J.Bravo <joepbravo@gmail.com>
2021-04-20 10:35:36 -0700

    Question 2

---
+++ question2.jl
@@ -0,0 +1,43 @@
+# 10,000 nonprofits with highert revenue as metric of size
+using Serialization
+using Statistics
+using SparseArrays
+
+summary_stats = Serialization.deserialize( "irs990extract.jldata" )
+termfreq = Serialization.deserialize( "termfreq.jldata" )
+terms = Serialization.deserialize( "terms.jldata" )
+
+funtion parse_rev( x )
+    rev = x[ "revenue" ]
+    if ismissing( rev )
+        0
+    else
+        parse( Int, x[ "revenue" ] )
+    end
+end
+
+revenue = map( parse_rev, summary_stats )
+highest_rev = sortperm( revenue, rev = true )
+top10k_indx = highest_rev[ 1: 10_000 ]
+#top10k = summary_stats[ top10k_indx, : ]
+top10k = summary_stats[ top10k_indx ]
+
+# Part 2
+# To find the nonprofit with the highest revenue
+top10k[1]
+
+# Part 3
+# Drop all words that don't appear at least twice in subset
+termfreq_2 = termfreq[ top10k_indx , : ]
+
+# To determine how frequqntly each term is represented in the subset.
+term_appearances = 0 .< termfreq_2
+word_count = sum( term_appearances, dims = 1 )
+
+
+fewerThanTwo = firstSubset .< 2
+twoOrMore = 2 .<= firstSubset
+
+# Update the termfreq matrix to remove letters that appear less than twice
+# Assign to new variable
+tf_10k = termfreq_2[ 1:end, twoOrMore ]
